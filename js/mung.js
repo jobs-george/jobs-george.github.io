aae1b8b386907533c8c0f5d4c2a243d25a9a236d
Jobs <jobs.george@protonmail.com>
2024-10-28 10:43:04 +0000

    changed layout to responsive topbar

--- js/mung.js
+++ js/mung.js
@@ -1,11 +1,13 @@
-function vgCbKyZRbIJyTzo() {
-  var OWhuqxLUjlYyJWk = ["106", "111", "x62", "x73", "46", "103", "101", "111", "x72", "x67", "101", "x40", "112", "x72", "111", "116", "x6f", "110", "109", "97", "105", "108", "46", "99", "x6f", "109"];
-  var sLdLhpQNJHGIvJC = ['?', 's', 'u', 'b', 'j', 'e', 'c', 't', '=', '&', 'c', 'c', '=', '&', 'b', 'c', 'c', '=', '&', 'b', 'o', 'd', 'y', '='];
-  var ihUWoMyahqVmHZn = ["x6a", "x6f", "98", "115", "x2e", "103", "101", "x6f", "114", "x67", "101", "64", "112", "114", "111", "x74", "111", "110", "109", "x61", "105", "x6c", "x2e", "x63", "111", "x6d"];
-  document.write("<a href=\"&#x6d;&#x61;&#x69;&#x6c;&#000116;&#111;&#x3a;");
-  for (i = 0; i < OWhuqxLUjlYyJWk.length; i++) document.write('&#' + OWhuqxLUjlYyJWk[i] + ';');
-  for (i = 0; i < sLdLhpQNJHGIvJC.length; i++) document.write(sLdLhpQNJHGIvJC[i]);
+// https://www.addressmunger.com/
+
+function jfdSYVOZCVkERLm() {
+  var NgWuOgdNrUWuZrO = ["109", "97", "105", "108", "116", "x6f", "x3a", "106", "x6f", "98", "115", "46", "x67", "101", "111", "x72", "x67", "101", "64", "x70", "x72", "111", "x74", "111", "x6e", "x6d", "97", "x69", "x6c", "x2e", "99", "x6f", "x6d"];
+  var zRKHzjzDVnNzOBJ = ['?', 's', 'u', 'b', 'j', 'e', 'c', 't', '=', '&', 'c', 'c', '=', '&', 'b', 'c', 'c', '=', '&', 'b', 'o', 'd', 'y', '='];
+  var UZliJimnyYKlPcv = ["67", "111", "110", "x74", "x61", "99", "116"];
+  document.write("<a href=\"&#x6d;&#97;&#x69;&#108;&#116;&#x6f;&#00058;");
+  for (i = 0; i < NgWuOgdNrUWuZrO.length; i++) document.write('&#' + NgWuOgdNrUWuZrO[i] + ';');
+  for (i = 0; i < zRKHzjzDVnNzOBJ.length; i++) document.write(zRKHzjzDVnNzOBJ[i]);
   document.write('" style="" class="" id="">');
-  for (i = 0; i < ihUWoMyahqVmHZn.length; i++) document.write('&#' + ihUWoMyahqVmHZn[i] + ';');
+  for (i = 0; i < UZliJimnyYKlPcv.length; i++) document.write('&#' + UZliJimnyYKlPcv[i] + ';');
   document.write('</a>');
 }
