f670583a8cec52722af672df3fcc38042ecfb28a
Jobs <jobs.george@protonmail.com>
2025-02-27 08:42:56 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Website
+
+Personal website written in HTML, CSS, and a sprinkle of JavaScript.
+
+# Getting Started
+
+The website can be run locally in the browser.
+
+# Build and Test
+
+## Development
+
+Run test builds in development branches with Netlify.
+
+## Validation
+
+External website link can be validated with the python script,
+
+```sh
+python3 app.py
+```
+
+HTML and CSS can be checked at:
+
+https://validator.w3.org/
+
+# Contribute
+
+N/A.
---
+++ js/mung.js
@@ -0,0 +1,13 @@
+// https://www.addressmunger.com/
+
+function jfdSYVOZCVkERLm() {
+  var NgWuOgdNrUWuZrO = ["109", "97", "105", "108", "116", "x6f", "x3a", "106", "x6f", "98", "115", "46", "x67", "101", "111", "x72", "x67", "101", "64", "x70", "x72", "111", "x74", "111", "x6e", "x6d", "97", "x69", "x6c", "x2e", "99", "x6f", "x6d"];
+  var zRKHzjzDVnNzOBJ = ['?', 's', 'u', 'b', 'j', 'e', 'c', 't', '=', '&', 'c', 'c', '=', '&', 'b', 'c', 'c', '=', '&', 'b', 'o', 'd', 'y', '='];
+  var UZliJimnyYKlPcv = ["67", "111", "110", "x74", "x61", "99", "116"];
+  document.write("<a href=\"&#x6d;&#97;&#x69;&#108;&#116;&#x6f;&#00058;");
+  for (i = 0; i < NgWuOgdNrUWuZrO.length; i++) document.write('&#' + NgWuOgdNrUWuZrO[i] + ';');
+  for (i = 0; i < zRKHzjzDVnNzOBJ.length; i++) document.write(zRKHzjzDVnNzOBJ[i]);
+  document.write('" style="" class="" id="">');
+  for (i = 0; i < UZliJimnyYKlPcv.length; i++) document.write('&#' + UZliJimnyYKlPcv[i] + ';');
+  document.write('</a>');
+}
