fe7bb3a958e2ca1e684b06dba47edde48e622966
Jobs <jobs.george@protonmail.com>
2025-09-05 16:52:02 +0100

    ran review

--- js/mung.js
+++
@@ -1,5 +0,0 @@
-// Simple email protection
-function showContact() {
-  var email = 'jobs.george' + String.fromCharCode(64) + 'protonmail.com';
-  document.write('<a href="mailto:' + email + '">Contact</a>');
-}
